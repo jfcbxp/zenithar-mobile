b2f3bd55603b65d6aa66bef4798f109effe20580
Joao Felipe <jfcbxp@gmail.com>
2023-02-01 20:20:29 -0300

    correção de code smell

--- src/pages/Discount/Confirmation/index.test.tsx
+++ src/pages/Discount/Confirmation/index.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import renderer, { act } from "react-test-renderer";
+import renderer from "react-test-renderer";
 
 import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
 import { StackParams } from "../../../types/stack.params";
--- src/pages/Discount/index.test.tsx
+++ src/pages/Discount/index.test.tsx
@@ -1,34 +1,33 @@
 import React from "react";
-import renderer, { act } from "react-test-renderer";
+import renderer from "react-test-renderer";
 
 import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
 import { StackParams } from "../../types/stack.params";
 import { RouteProp, useNavigation } from "@react-navigation/native";
 import Discount from ".";
 
-let mockNavigation: StackScreenProps<
-    StackParams,
-    "Discount",
-    undefined
->;
+let mockNavigation: StackScreenProps<StackParams, "Discount", undefined>;
 
 let route: RouteProp<StackParams, "Discount">;
 
 beforeAll(() => {
-    let navigation =
-        useNavigation<StackNavigationProp<StackParams, "Discount">>();
-    route = useNavigation<RouteProp<StackParams, "Discount">>();
-    mockNavigation = {
-        navigation: navigation,
-        route: route,
-    };
+  let navigation =
+    useNavigation<StackNavigationProp<StackParams, "Discount">>();
+  route = useNavigation<RouteProp<StackParams, "Discount">>();
+  mockNavigation = {
+    navigation: navigation,
+    route: route,
+  };
 });
 
 it("Discount renders without crashing", async () => {
-    const rendered = renderer.create(
-        <Discount
-            navigation={mockNavigation.navigation}
-            route={mockNavigation.route} />
-    ).toJSON()
-    expect(rendered).toBeTruthy()
-})
+  const rendered = renderer
+    .create(
+      <Discount
+        navigation={mockNavigation.navigation}
+        route={mockNavigation.route}
+      />
+    )
+    .toJSON();
+  expect(rendered).toBeTruthy();
+});
