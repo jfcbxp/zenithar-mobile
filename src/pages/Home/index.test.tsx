9b756b40db1e5d7e1340f3dcec0629747a288816
Speciallist <fhilype@hotmail.com>
2023-02-02 15:19:24 -0300

    Correções nos dropdowns;
    Andamento da tarefa aumentar a cobertura dos testes;

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import renderer, { act } from "react-test-renderer";
 
 import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
 import { StackParams } from "../../types/stack.params";
 import { RouteProp, useNavigation } from "@react-navigation/native";
 import Home from ".";
+import { NavigationButton } from "../../components/buttons/navigation-button";
 
 let mockNavigation: StackScreenProps<StackParams, "Home", undefined>;
 
@@ -26,7 +27,6 @@ it("Home renders without crashing", () => {
         navigation={mockNavigation.navigation}
         route={mockNavigation.route}
       />
-    )
-    .toJSON();
+    ).toJSON();
   expect(rendered).toBeTruthy();
 });
