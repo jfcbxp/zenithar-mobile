f2ea438cadd7c5e198e61e4b90a47d4ac8ec78f0
Joao Felipe <jfcbxp@gmail.com>
2023-02-05 16:35:59 -0300

    adicionado babel-plugin-inline-dotenv

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,9 @@ module.exports = function (api) {
   return {
     presets: [
       'babel-preset-expo',
-      ['@babel/preset-env', { targets: { node: 'current' } }],
       '@babel/preset-typescript',
     ],
     plugins: [
-      'module:react-native-dotenv',
       ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
     ],
   };
--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -2,30 +2,20 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/database";
-import "firebase/compat/storage"
-import {
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
-  MEASUREMENT_ID,
-  DATABASE_URL,
-} from "@env";
+import "firebase/compat/storage";
 import { initializeAuth } from "firebase/auth";
 import { getReactNativePersistence } from "firebase/auth/react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
-  databaseURL: DATABASE_URL,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+  databaseURL: process.env.DATABASE_URL,
 };
 
 export const fireApp = firebase.initializeApp(firebaseConfig);
