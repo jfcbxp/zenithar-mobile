8b02428dd0101f6c01f08cb984c8a0dec7b7111b
Joao Felipe <jfcbxp@gmail.com>
2023-02-06 10:54:32 -0300

    remoção de testes desnecessarios

--- src/pages/Discount/index.test.tsx
+++ src/pages/Discount/index.test.tsx
@@ -8,6 +8,7 @@ import Discount from ".";
 import { HeaderDropdown } from "../../components/dropdowns/discount/header-dropdown";
 import { ItemsDropdown } from "../../components/dropdowns/discount/items-dropdown";
 import { PaymentMethodDropdown } from "../../components/dropdowns/discount/payment-method-dropdown";
+import { Pressable } from "react-native";
 
 let mockNavigation: StackScreenProps<StackParams, "Discount", undefined>;
 
@@ -53,4 +54,62 @@ it("Discount test Dropdowns", async () => {
   expect(headerDropdown).toBeTruthy();
   expect(itemsDropdown).toBeTruthy();
   expect(paymentMethodDropdown).toBeTruthy();
+});
+
+it("HeaderDropdown with Pressable", async () => {
+  const rendered = renderer.create(
+    <Discount
+      navigation={mockNavigation.navigation}
+      route={mockNavigation.route}
+    />
+  );
+  const headerDropdown = rendered.root.findByType(HeaderDropdown);
+
+  const pressable = headerDropdown.findByType(Pressable);
+
+  const icon = headerDropdown.findAllByProps({ testID: "icon" })[0];
+  expect(icon.props.name).toBe("chevron-down");
+
+  await act(() => pressable.props.onPress());
+
+  expect(icon.props.name).toBe("chevron-up");
+});
+
+it("ItemsDropdown with Pressable", async () => {
+  const rendered = renderer.create(
+    <Discount
+      navigation={mockNavigation.navigation}
+      route={mockNavigation.route}
+    />
+  );
+
+  const itemsDropdown = rendered.root.findByType(ItemsDropdown);
+
+  const pressable = itemsDropdown.findByType(Pressable);
+
+  const icon = itemsDropdown.findAllByProps({ testID: "icon" })[0];
+  expect(icon.props.name).toBe("chevron-down");
+
+  await act(() => pressable.props.onPress());
+
+  expect(icon.props.name).toBe("chevron-up");
+});
+
+it("PaymentMethodDropdown with Pressable", async () => {
+  const rendered = renderer.create(
+    <Discount
+      navigation={mockNavigation.navigation}
+      route={mockNavigation.route}
+    />
+  );
+  const paymentMethodDropdown = rendered.root.findByType(PaymentMethodDropdown);
+
+  const pressable = paymentMethodDropdown.findByType(Pressable);
+
+  const icon = paymentMethodDropdown.findAllByProps({ testID: "icon" })[0];
+  expect(icon.props.name).toBe("chevron-down");
+
+  await act(() => pressable.props.onPress());
+
+  expect(icon.props.name).toBe("chevron-up");
 });
--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -7,18 +7,65 @@ import { RouteProp, useNavigation } from "@react-navigation/native";
 import Home from ".";
 import { NavigationButton } from "../../components/buttons/navigation-button";
 import { DiscountModal } from "../../components/modals/discount";
-import { Modal } from "react-native";
+import { FlatList, Modal, Pressable } from "react-native";
 import { TextInput } from "../../components/inputs/text-input";
 import { Button } from "../../components/buttons/button";
 import { HomeContainer } from "../../components/containers/home-container";
 import { Logs } from "../../components/lists/logs";
 import { LogsItem } from "../../components/lists/logs-item";
-import { MaterialIcons as Icon } from '@expo/vector-icons';
 import { Picker } from "../../components/pickers/picker";
+import { Header } from "../../components/headers/header";
+import { UserSettings } from "../../components/modals/user-settings";
+import { Portrait } from "../../components/portrait/portrait";
+import { FullNameInput } from "../../components/inputs/fullname-input";
+import { CommandLink } from "../../components/buttons/command-link";
+import { PasswordInput } from "../../components/inputs/password-input";
+import { AuthContext } from "../../contexts/auth.provider";
+import { User } from "../../models/user.model";
 
 let mockNavigation: StackScreenProps<StackParams, "Home", undefined>;
 
 let route: RouteProp<StackParams, "Home">;
+
+const company = "companyTest";
+const department = "departmentTest";
+const loading = false;
+const signUp = jest.fn();
+const signIn = jest.fn();
+const signOut = jest.fn();
+const recoverPassword = jest.fn();
+const userUpdate = jest.fn();
+const user: User = {
+  uid: "uuidTest",
+  email: "emailTest",
+  fullName: "fullNameTest",
+  company: "companyTest",
+  department: "departmentTest",
+  verified: true,
+  portrait: "portraitTest",
+  branches: [
+    {
+      id: "1",
+      name: "Test",
+    },
+  ],
+  logs: [
+    {
+      id: "1",
+      date: "Test",
+      title: "Test",
+      description: "Test",
+      type: "LIBERACAO_ORCAMENTO",
+    },
+    {
+      id: "2",
+      date: "Test 2",
+      title: "Test 2",
+      description: "Test 2",
+      type: "DESCONTO_ORCAMENTO",
+    },
+  ],
+};
 
 beforeAll(() => {
   let navigation = useNavigation<StackNavigationProp<StackParams, "Home">>();
@@ -30,73 +77,181 @@ beforeAll(() => {
 });
 
 it("Home renders without crashing", () => {
-  const rendered = renderer.create(
-    <Home
-      navigation={mockNavigation.navigation}
-      route={mockNavigation.route} />
-  ).toJSON();
+  const rendered = renderer
+    .create(
+      <AuthContext.Provider
+        value={{
+          user,
+          company,
+          department,
+          loading,
+          signUp,
+          signIn,
+          signOut,
+          recoverPassword,
+          userUpdate,
+        }}
+      >
+        <Home
+          navigation={mockNavigation.navigation}
+          route={mockNavigation.route}
+        />
+      </AuthContext.Provider>
+    )
+    .toJSON();
   expect(rendered).toBeTruthy();
 });
 
-it("Home test HomeContainer, Logs, LogsItem", () => {
+it("Home test Header", async () => {
+  jest.mock("expo-image-picker", () => {
+    return jest.fn().mockImplementation(() => {
+      return {
+        launchImageLibraryAsync: async () => {
+          return {
+            canceled: true,
+            assets: [{ uri: "teste" }],
+          };
+        },
+      };
+    });
+  });
   const rendered = renderer.create(
-    <Home
-      navigation={mockNavigation.navigation}
-      route={mockNavigation.route} />
-  )
+    <AuthContext.Provider
+      value={{
+        user,
+        company,
+        department,
+        loading,
+        signUp,
+        signIn,
+        signOut,
+        recoverPassword,
+        userUpdate,
+      }}
+    >
+      <Home
+        navigation={mockNavigation.navigation}
+        route={mockNavigation.route}
+      />
+    </AuthContext.Provider>
+  );
 
-  const homeContainer = rendered.root.findByType(HomeContainer)
-  expect(homeContainer.props.title).toBe("Histórico")
-  expect(homeContainer.props.children).toBeDefined()
+  const header = rendered.root.findByType(Header);
 
-  const DATA: Array<any> = [{
-    id: "1",
-    date: "Test",
-    title: "Test",
-    description: "Test",
-    type: "LIBERACAO_ORCAMENTO"
-  }]
+  const pressable = header.findByType(Pressable);
+  await act(() => pressable.props.onPress());
 
-  const _rendered = renderer.create(
-    <Logs data={DATA} />
-  )
+  const userSettings = header.findByType(UserSettings);
+  expect(userSettings.props.visible).toBe(true);
 
-  const logs = _rendered.root.findByType(Logs)
-  expect(logs.props.data).toBeDefined()
+  const modal = header.findByType(Modal);
+  expect(modal.props.visible).toBe(true);
 
-  const logsItem = _rendered.root.findByType(LogsItem)
-  expect(logsItem.props.data).toBeDefined()
+  const portrait = header.findByType(Portrait);
+  expect(portrait.props.source).toBe("portraitTest");
 
-  const icon = _rendered.root.findByType(Icon)
-  expect(icon.props.name).toBe("")
-})
+  const fullNameInput = header.findByType(FullNameInput);
+  await act(() => fullNameInput.props.onChangeText("Bruce Wayne"));
+  expect(fullNameInput.props.value).toBe("Bruce Wayne");
+
+  const commandLink = header.findByType(CommandLink);
+  await act(() => commandLink.props.onPress());
+
+  const passwordInput = header.findAllByType(PasswordInput).length;
+  expect(passwordInput).toBe(3);
+
+  const _passwordInput = header.findAllByType(PasswordInput);
+
+  const password1 = _passwordInput[0];
+  await act(() => password1.props.onChangeText("965874"));
+  expect(password1.props.value).toBe("965874");
+
+  const password2 = _passwordInput[1];
+  await act(() => password2.props.onChangeText("965874"));
+  expect(password2.props.value).toBe("965874");
+
+  const password3 = _passwordInput[2];
+  await act(() => password3.props.onChangeText("965874"));
+  expect(password3.props.value).toBe("965874");
+
+  const button = header.findAllByType(Button).length;
+  expect(button).toBe(3);
+
+  const _button = header.findAllByType(Button);
+
+  const button1 = _button[0];
+  expect(button1.props.title).toBe("SELECIONAR");
+
+  const button2 = _button[1];
+  expect(button2.props.title).toBe("CANCELAR");
+
+  const button3 = _button[2];
+  await act(() => button3.props.onPressIn());
+  expect(button3.props.title).toBe("CONFIRMAR");
+  expect(button3.props.disabled).toBe(true);
+});
+
+it("Home test HomeContainer, Logs, LogsItem", async () => {
+  const rendered = renderer.create(
+    <AuthContext.Provider
+      value={{
+        user,
+        company,
+        department,
+        loading,
+        signUp,
+        signIn,
+        signOut,
+        recoverPassword,
+        userUpdate,
+      }}
+    >
+      <Home
+        navigation={mockNavigation.navigation}
+        route={mockNavigation.route}
+      />
+    </AuthContext.Provider>
+  );
+
+  const homeContainer = rendered.root.findByType(HomeContainer);
+  expect(homeContainer.props.title).toBe("Histórico");
+  expect(homeContainer.props.children).toBeDefined();
+
+  const logs = homeContainer.findByType(Logs);
+  expect(logs.props.data).toBeDefined();
+
+  const flatListLog = logs.findByType(FlatList);
+
+  expect(flatListLog.props.data).toBeDefined();
+
+  const logsItem = flatListLog.findAllByType(LogsItem).length;
+  expect(logsItem).toBe(2);
+});
 
 it("Home test DiscountModal", async () => {
   const rendered = renderer.create(
-    <Home
-      navigation={mockNavigation.navigation}
-      route={mockNavigation.route} />
-  )
+    <Home navigation={mockNavigation.navigation} route={mockNavigation.route} />
+  );
 
-  const navigationButton = rendered.root.findByType(NavigationButton)
-  await act(() => navigationButton.props.onPress())
+  const navigationButton = rendered.root.findByType(NavigationButton);
+  await act(() => navigationButton.props.onPress());
 
-  const discountModal = rendered.root.findByType(DiscountModal)
-  expect(discountModal.props.visible).toBe(true)
+  const discountModal = rendered.root.findByType(DiscountModal);
+  expect(discountModal.props.visible).toBe(true);
 
-  const modal = rendered.root.findAllByType(Modal)
-  const modal1 = modal[0]
-  expect(modal1.props.visible).toBe(true)
+  const modal = rendered.root.findAllByType(Modal);
+  const modal1 = modal[0];
+  expect(modal1.props.visible).toBe(true);
 
-  const textInput = rendered.root.findByType(TextInput)
-  await act(() => textInput.props.onChangeText("965874"))
-  expect(textInput.props.value).toBe("965874")
+  const textInput = rendered.root.findByType(TextInput);
+  await act(() => textInput.props.onChangeText("965874"));
+  expect(textInput.props.value).toBe("965874");
 
-  const button = rendered.root.findByType(Button)
-  expect(button.props.title).toBe("CONTINUAR")
-  expect(button.props.disabled).toBe(true)
+  const button = rendered.root.findByType(Button);
+  expect(button.props.title).toBe("CONTINUAR");
+  expect(button.props.disabled).toBe(true);
 
-  const picker = rendered.root.findByType(Picker)
-  await act(()=> picker.props.setValue(""))
-  expect(picker.props.value).toBe("")
-})
+  const picker = rendered.root.findByType(Picker);
+  await act(() => picker.props.setValue(""));
+  expect(picker.props.value).toBe("");
+});
