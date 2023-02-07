e994585c6abfd559900f5e4afefe412514942aff
Joao Felipe <jfcbxp@gmail.com>
2023-02-07 17:56:12 -0300

    adicionar service-worker para pwa (#80)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, Platform } from "react-native";
 import "react-native-gesture-handler";
 import AuthProvider from "./src/contexts/auth.provider";
 import Routes from "./src/routes";
 import { NavigationContainer } from "@react-navigation/native";
-
+import * as serviceWorkerRegistration from "./src/serviceWorkerRegistration";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
@@ -23,4 +23,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#212A4D",
   },
-})
+});
+
+if (Platform.OS === "web") {
+  serviceWorkerRegistration.register();
+}
