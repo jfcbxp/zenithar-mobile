66ac659b60b84b1c5439ca73cfe3320552bfc0e8
Joao Braga <jfcbxp@gmail.com>
2023-02-27 11:36:08 -0300

    teste 6

--- app.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-  if (process.env.EXPO_ENV === "production") {
-    return require("./app.production.json");
-  } else {
-    return require("./app.development.json");
-  }
-};
